e9c0d98dc5a1a2a472e1ef5a6579d912adfa439e
Daniel Villajuan <danielvillajuan@hotmail.com>
2025-05-20 21:07:15 -0300

    Fin de clase 2

---
+++ Clase-2/index.js
@@ -0,0 +1,130 @@
+
+
+// const edad = prompt("Cuantos años tenes?") // "18" 
+// const edadNumerico = parseInt(edad) 
+
+// console.log("Buenas tu edad es:" + edadNumerico)
+
+// if(edadNumerico >= 18){
+//     console.log(" Felicitaciones sos mayor de edad")
+// }else {
+//     console.log("No podes tomar alcohol mal ahi.")
+// }
+
+// if(edadNumerico <= 50){
+//     if (edadNumerico <= 18){
+//         console.log(" Sos adolecente")
+//     }else {
+//         console.log("Sos adulto")
+//     }
+// }  else {
+//     console.log("Sos adulto mayor")
+// }
+
+
+
+// console.log("fin del programa")
+
+// const nombre = prompt("Cual es tu nombre?")
+// const apellido = prompt("Cual es tu apellido?")
+// const edad = 21
+// if (nombre !== '' ){
+//     if(apellido !== ''){
+//         console.log("Hola " + nombre + " " + apellido)
+//     }else {
+//         console.log("Alguno de los 2 valores no se pasaron")
+//     }
+// }else {
+//     console.log("Alguno de los 2 valores no se pasaron")
+// }
+
+
+
+// 
+// if (nombre && apellido ){
+//     console.log("Hola " + nombre + " " + apellido)
+// }else {
+//     console.log("Alguno de los 2 valores no se pasaron")
+// }
+
+// console.log(undefined)
+
+// operador AND ( && )
+// operador OR ( || )
+
+// if(null) // falso
+// if(undefined) // falso
+
+// const edad = parseInt(prompt("Ingrese edad")) ?? 18
+// console.log(edad) //
+
+// "Daniel" || "Nombre generico"
+// "" || "Hola" 
+// 0 || 10
+// NaN || 20
+// null || 30
+// undefined || 20
+
+// "Daniel" ?? "Nombre generico"
+// "" ?? "Hola" 
+// 0 ?? 10
+// NaN ?? 20
+// null ?? 30
+// undefined ?? 20
+
+
+
+
+
+// if(){
+
+// }else if(){
+
+// }else if(){
+    
+// } else {
+
+// }
+
+
+// switch (true){ // true
+//     case edad <= 18: 
+//         console.log("Sos adolecente");
+//         break;
+//     case edad <= 50:
+//         console.log("Sos adulto")
+//         break;
+//     case edad <= 80:
+//         console.log("Sos de la 3ra edad")
+//         break;
+//     default:
+//         console.log("Sos legendario")    
+// }
+
+// i = i + 1 
+// FOR (contable)
+
+for(let i=1; i <= 10 ; i++){
+    // pregunto la nota
+    // guardo la nota y lo voy acumulando
+    console.log(i)
+
+}
+
+// 1- Hacer un programa que permita ingresar 10 notas de alumno y sacar el promedio.
+// 2- Modificar para que el usuario primero ingrese la cantidad de alumnos y luego sacar el promedio en base a eso.
+// 3- Modificar que siga haciendo lo del punto 2 pero que a medida de ingrsar la nota mencione si esta aprobado o no (>=7)
+// 4- Modificar el for del punto 3, pasarlo a while
+
+console.log('fin del programa')
+
+
+// while (condicional)
+
+let i = 0;
+
+while(i < 10){
+    console.log(i)
+    i++
+}
+// fuera del while
