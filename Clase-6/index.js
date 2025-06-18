7e39f985d98a7e1a8d8a158f2b5d2bec4dee5a33
Daniel Villajuan <danielvillajuan@hotmail.com>
2025-06-17 21:11:31 -0300

    Fin de clase 6

--- Clase-6/index.js
+++ Clase-6/index.js
@@ -54,3 +54,113 @@
 // validarDNI("33888777") // true
 
 
+// const productos = [
+//     { nombre: "Arroz", precio: 1500, categoria: "Almacen" }, // false
+//     { nombre: "Fideos", precio: 800, categoria: "Almacen" }, // false
+//     { nombre: "Cocacola", precio: 2500, categoria: "Bebida" }, // true 
+//     { nombre: "Agua Mineral", precio: 1500, categoria: "Bebida" },
+//     { nombre: "Carne", precio: 8500, categoria: "Congelados" },
+// ]
+
+// forEach - no retorna nada
+
+// let contador = 0
+
+// productos.forEach((elemento) => {
+
+//     if(elemento.precio > 1500){
+//         console.log(elemento)
+//         contador++
+//     }
+// })
+
+// console.log('hay ', contador, ' productos mayores a 1500')
+// console.log(`Hay ${contador} productos mayores a 1500`)
+
+
+// find -> devuelve un elemento en caso de encontrarlo, sino undefined
+
+// const productoEncontrado = productos.find((elemento) => {
+//     return elemento.nombre === 'Coca cola'
+// })
+
+// console.log(productoEncontrado)
+
+
+// FILTER -> filtra un array en base a una condicion -> siempre devolvera un ARRAY
+
+// const resultado = productos.filter((elemento) => {
+//     return elemento.precio >= 1000 && elemento.precio <= 1500
+// })
+
+// console.log(resultado)
+
+// SOME -> simplemente devuelve un booleano , true si existe ( cumple la condicion) false si nunca se cumple
+
+
+// const resultado = productos.some((elemento) => {
+//      return elemento.nombre === "Coca cola"
+// })
+
+// console.log(resultado)
+
+
+// const alumno = [
+//     { nombre: "Miguel", nota: 6, activo: true},
+//     { nombre: "Nico", nota: 4, activo: true},
+//     { nombre: "Rober", nota: 8, activo: true},
+//     { nombre: "Marlene", nota: 7, activo: true},
+// ]
+
+
+// // const resultado = alumno.map((elemento) => {
+
+// //     if(elemento.nota >= 7){
+// //         return {
+// //             ...elemento,
+// //             aprobado: true
+// //         }
+// //     }else {   
+// //         return {
+// //             ...elemento,
+// //             aprobado: false
+// //         }
+// //     }
+// // }) // devuelve un nuevo array
+
+// // console.log(resultado)
+
+// const resultado = alumno.reduce((acum, elemento) => {
+//     return elemento.nota + acum
+// }, 0) // retorna un valor/variable
+
+// console.log(resultado)
+
+
+const productos = [
+    { nombre: "Arroz", precio: 1500, categoria: "Almacen" }, // false
+    { nombre: "Fideos", precio: 800, categoria: "Almacen" }, // false
+    { nombre: "Cocacola", precio: 2500, categoria: "Bebida" }, // true 
+    { nombre: "Agua Mineral", precio: 1500, categoria: "Bebida" },
+    { nombre: "Carne", precio: 8500, categoria: "Congelados" },
+    { nombre: "Pescado", precio: 5500, categoria: "Congelados" },
+]
+
+const resultado = productos.reduce((acum, elemento) => {
+    // acum  -> {}
+
+    if(!acum[elemento.categoria]){ // acum.Almacen -> undefined
+        return {
+            ...acum,
+            [elemento.categoria]: [{...elemento}] 
+        }
+    }
+
+    acum[elemento.categoria].push(elemento)
+    return acum
+
+
+
+}, {})
+
+console.log(resultado)
